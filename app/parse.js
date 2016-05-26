6db131493e667a5d60cb02c6c70bfd0da5870366
Robert Plant <robert.plant@sykescottages.co.uk>
2016-05-26 19:23:13 +0100

    cpu bar chart

--- app/parse.js
+++ app/parse.js
@@ -1,14 +1,48 @@
 'use strict';
 
-const selector = require('./selectors.js');
-const os     = require('os');
-const exec   = require('child_process').exec;
-const disk   = require('diskusage');
-const moment = require('moment');
+const selector    = require('./selectors.js');
+const os          = require('os');
+const exec        = require('child_process').exec;
+const disk        = require('diskusage');
+const moment      = require('moment');
+const c3          = require('c3');
+const libCpuUsage = require('cpu-usage');
 
 $(document).ready(function () {
+    var i = 0;
+    $('.value[data-format="chart"]').each(function () {
+        i++;
+        var type = $(this).parent().attr('class');
+        var id = `#${type}-chart-${i}`;
+        $(this).html(`<div id="${type}-chart-${i}"></div>`);
+        var chart = c3.generate({
+            bindto: id,
+            data  : {
+                columns: [
+                    ['data', 0]
+                ],
+                type   : 'gauge'
+            },
+            color : {
+                pattern  : ['#60B044', '#F6C600', '#F97600', '#FF0000' ],
+                threshold: {
+                    values: [30, 60, 90, 100]
+                }
+            }
+        });
+        $(id).data('c3-chart', chart)
+    });
+
+    libCpuUsage(1000, function (load) {
+        $('[id^="cpu-chart"].c3').each(function() {
+            $(this).data('c3-chart').load({
+                columns: [['data', load]]
+            });
+        });
+    });
+
     setInterval(
-        function () {
+        () => {
             selector.hostname.text(os.hostname());
 
             selector.memory.find('.value').each(function () {
@@ -24,10 +58,12 @@ $(document).ready(function () {
             });
             selector.diskusage.find('.value').each(function () {
                 disk.check($('.disk-usage').data('disk'), (err, info) => {
-                    $(this).text(
-                        format[$(this).data('format')](info.available) + ' / ' + format[$(this)
-                            .data('format')](info.total)
-                    )
+                    var selectedFormat = $(this).data('format');
+                    if (typeof format[selectedFormat] != "undefined") {
+                        $(this).text(
+                            format[selectedFormat](info.available) + ' / ' + format[selectedFormat](info.total)
+                        )
+                    }
                 });
             });
 
@@ -40,7 +76,6 @@ $(document).ready(function () {
                     $(this).text(stdout);
                 });
             });
-
         },
         1000
     );

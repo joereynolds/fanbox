281c3e61a259c3748a648ec331cd8368fc9b428b
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2016-05-26 12:17:46 +0100

    add boilerplate

---
+++ app/main.js
@@ -0,0 +1,35 @@
+'use strict'
+
+
+const electron = require('electron');
+
+//Module to control application life
+const app = electron.app;
+
+//Module to create a native browser window
+const BrowserWindow = electron.BrowserWindow;
+
+
+//global reference to the window object. This is needed otherwise it
+//gets garbage collected
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+    //Macs handle things differently
+    if (process.playform != 'darwin') {
+        app.quit();
+    }
+});
+
+
+app.on('ready', function() {
+
+    //width and height should be configurable by the user. Change this!
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.loadURL('file://' + __dirname + 'fanbox.html');
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+});
+
